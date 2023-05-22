872b81909ae597a9b6ce73253cfab55be06ae1ec
Yaroslav Yakovenko <111749401+Yaroslav-Yaroslav@users.noreply.github.com>
2023-05-22 10:57:18 +0300

    Update MovieItem.jsx

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,6 +1,6 @@
 import { ContentWrap, FlexWrap, Img } from './MovieItem.styled';
 import { Title } from 'pages/Home/Home.styled';
-
+import PropTypes from 'prop-types';
 const MovieItem = ({ movie }) => {
   const {
     original_title,
@@ -31,6 +31,21 @@ const MovieItem = ({ movie }) => {
       </ContentWrap>
     </FlexWrap>
   );
+};
+
+MovieItem.propTypes = {
+  movie: PropTypes.shape({
+    original_title: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+      }).isRequired
+    ).isRequired,
+    overview: PropTypes.string.isRequired,
+    vote_average: PropTypes.number.isRequired,
+    poster_path: PropTypes.string.isRequired,
+    release_date: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default MovieItem;
